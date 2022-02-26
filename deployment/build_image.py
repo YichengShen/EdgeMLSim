97c9b01e5aff6fd996239fbc917046de127158fe
YichengShen <jsszsyc@hotmail.com>
2022-02-26 21:18:57 +0800

    about registry

--- deployment/build_image.py
+++ deployment/build_image.py
@@ -1,3 +1,4 @@
+from time import sleep
 import docker
 
 
@@ -10,21 +11,31 @@ def build_image(client):
 
     # Run a local registry for sharing the image
     registry_image = client.images.pull("registry:2")
-    registry_container = client.api.create_container(registry_image,
-                                                     name="registry",
-                                                     detach=True,
-                                                     ports=[5000],
-                                                     # restart_policy={"Name": "always"},
-                                                     host_config=client.api.create_host_config(port_bindings={
-                                                         5000: 5000
-                                                     }))
+    try:
+        registry_container = client.containers.get("registry")
+    except docker.errors.NotFound as exc:
+        print(f"Check container name!\n{exc.explanation}")
+    else:
+        client.api.remove_container("registry", force=True)
+    client.containers.prune()
+    registry_list = client.api.create_container(registry_image.id,
+                                                name="registry",
+                                                detach=True,
+                                                ports=[5000],
+                                                host_config=client.api.create_host_config(port_bindings={
+                                                    5000: 5000
+                                                }))
+    registry_container = client.containers.get(registry_list["Id"])
     registry_container.start()
     print("Local registry created")
 
+    sleep(2)  # Wait for the container to start
+
     # Push image to local registry
-    image_obj.tag("localhost:5000/edgemlsim")
-    for line in client.images.push("localhost:5000/edgemlsim", stream=True, decode=True):
+    image_tag = "localhost:5000/edgemlsim"
+    image_obj.tag(image_tag)
+    for line in client.images.push(image_tag, stream=True, decode=True):
         print(line)
     print("Image pushed to local registry\n")
 
-    return image_id
+    return image_id, image_tag
--- deployment/run.py
+++ deployment/run.py
@@ -19,41 +19,43 @@ print("New IP config generated")
 
 client = docker.from_env()
 
-image_id = build_image(client)
+image_id, image_tag = build_image(client)
 
-# # Create overlay network
-# ipam_pool = docker.types.IPAMPool(
-#     subnet='192.168.0.0/24', gateway='192.168.0.1')
-# ipam_config = docker.types.IPAMConfig(pool_configs=[ipam_pool])
-# overlay_net = client.networks.create(
-#     "overlay_net", driver="overlay", ipam=ipam_config, attachable=True)
+# Create overlay network
+ipam_pool = docker.types.IPAMPool(
+    subnet='192.168.0.0/24', gateway='192.168.0.1')
+ipam_config = docker.types.IPAMConfig(pool_configs=[ipam_pool])
+overlay_net = client.networks.create(
+    "overlay_net", driver="overlay", ipam=ipam_config, attachable=True)
 
-# # Run the Simulator container
+# Run the Simulator container
 # simulator = client.containers.create(
 #     image_id, name="simulator", command="python3 Simulator.py", detach=True, tty=True)
-# overlay_net.connect(simulator, ipv4_address=ip_config['ip_sim'])
-# simulator.start()
+simulator = client.containers.create(
+    image_tag, name="simulator", command="python3 Simulator.py", detach=True, tty=True)
+overlay_net.connect(simulator, ipv4_address=ip_config['ip_sim'])
+simulator.start()
 
-# sleep(10)
+sleep(10)
 
-# # Run the Cloud Server container
-# cloud_server = client.containers.create(
-#     image_id, name="cloud", command="python3 CloudServer.py", detach=True, tty=True)
-# overlay_net.connect(cloud_server, ipv4_address=ip_config['ip_cloud'])
-# cloud_server.start()
+# Run the Cloud Server container
+cloud_server = client.containers.create(
+    image_id, name="cloud", command="python3 CloudServer.py", detach=True, tty=True)
+overlay_net.connect(cloud_server, ipv4_address=ip_config['ip_cloud'])
+cloud_server.start()
 
-# sleep(10)
+sleep(10)
 
-# # Run Edge Server containers
-# edge_servers = []
-# for idx in range(cfg['num_edges']):
-#     edge_server = client.containers.create(image_id, name="edge{idx}".format(
-#         idx=idx), command="python3 EdgeServer.py --ip_index {idx}".format(idx=idx), detach=True, tty=True)
-#     overlay_net.connect(edge_server, ipv4_address=ip_config['ip_edges'][idx])
-#     edge_server.start()
-#     edge_servers.append(edge_server)
+# Run Edge Server containers
+edge_servers = []
+for idx in range(cfg['num_edges']):
+    edge_server = client.containers.create(image_id, name="edge{idx}".format(
+        idx=idx), command="python3 EdgeServer.py --ip_index {idx}".format(idx=idx), detach=True, tty=True)
+    overlay_net.connect(edge_server, ipv4_address=ip_config['ip_edges'][idx])
+    edge_server.start()
+    edge_servers.append(edge_server)
 
-# sleep(10)
+sleep(10)
 
 # # Run Worker as services
 # workers = []
